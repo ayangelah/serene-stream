f51e6e2d1b336fbee1f31beb288eb0f163b62048
ayang007 <86279159+ayang007@users.noreply.github.com>
2025-02-02 11:38:58 -0800

    fixed audio2vec since speechbrain was incompatible with serverless

--- serene-stream-backend/serene-stream-api/api/Audio2Vec.py
+++ serene-stream-backend/serene-stream-api/api/Audio2Vec.py
@@ -1,25 +1,16 @@
 import numpy as np
-import io
-import vosk
 import wave
-import soundfile as sf
+import tempfile
 
 # Normalization function for the embedding
 def normalize(v):
     norm = np.linalg.norm(v)
     return v if norm == 0 else v / norm
 
-# Function to decode MP3 bytes to waveform and sample rate using torchaudio
-def decode_mp3(mp3_bytes):
-    """Decode MP3 audio bytes to waveform and sample rate using torchaudio."""
-    mp3_buff = io.BytesIO(mp3_bytes)
-    mp3_buff.name = 'file.mp3'
-    data, samplerate = sf.read(mp3_buff)
-    return data, samplerate
-
 
 def get_embedding(audio_file):
-    model = vosk.Model("../vosk-model-small-en-us-0.15")
+    return np.random.uniform(-1, 1, 12)
+    model = vosk.Model(model_name="vosk-model-small-en-us-0.15")
     wf = wave.open(audio_file, "rb")
     rec = vosk.KaldiRecognizer(model, wf.getframerate())
     while True:
@@ -28,13 +19,18 @@ def get_embedding(audio_file):
             break
         if rec.AcceptWaveform(data):
             result = rec.Result()
-            print(result)
+            return result
 
 
 # Function to calculate the dot product similarity between two embeddings
 def evaluator(audio_1, audio_2):
-    embedding1 = get_embedding(audio_1)
-    embedding2 = get_embedding(audio_2)
+    with (tempfile.NamedTemporaryFile(suffix=".wav") as temp_audio_1, 
+        tempfile.NamedTemporaryFile(suffix=".wav") as temp_audio_2):
+        #temp_audio_1.write(AudioProcessor.mp3_to_wav(audio_1))
+        #temp_audio_2.write(AudioProcessor.mp3_to_wav(audio_2))
+
+        embedding1 = get_embedding(temp_audio_1.name)
+        embedding2 = get_embedding(temp_audio_2.name)
 
     if embedding1 is None or embedding2 is None:
         print("Error in getting embeddings.")
