fd3b4f3825c6cf3221ca72bc598aca0189dc34bd
jaczhi <54284925+jaczhi@users.noreply.github.com>
2025-02-01 16:35:24 -0800

    mongo connection yipee

--- serene-stream-backend/serene-stream-api/api/index.py
+++ serene-stream-backend/serene-stream-api/api/index.py
@@ -1,10 +1,24 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+from pymongo import MongoClient
 
+load_dotenv()
+db_conn = os.getenv('DB_CONN')
 app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient(db_conn)
+db = client.serene_stream_db
+collection = db.mongo_test
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    # Get the first document from the collection
+    document = collection.find_one()
+    if document and 'testField' in document:
+        return document['testField']
+    return 'No data found'
 
 @app.route('/about')
 def about():
